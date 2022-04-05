7ecae015fa7ebb5c463b4c33af5a52b21796d38b
Gianluigi Liguori <gianluigiliguori@live.it>
2022-04-06 00:07:00 +0200

    Add middlware basic logic

--- WebUtils.HttpLogging.Sample/Program.cs
+++ WebUtils.HttpLogging.Sample/Program.cs
@@ -1,6 +1,15 @@
+using WebUtils.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseWebUtilsHttpLoggingFull();
+
+app.Map("/{**path}",
+    (HttpRequest request, HttpResponse response) =>
+    {
+        return "Hello World!";
+    }
+);
 
 app.Run();
--- WebUtils.HttpLogging/HttpLoggingMiddleware.cs
+++ WebUtils.HttpLogging/HttpLoggingMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace WebUtils.HttpLogging
 {
     internal sealed class HttpLoggingMiddleware
     {
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public HttpLoggingMiddleware(RequestDelegate next,
+                                         ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<HttpLoggingMiddleware>();
+
+
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Call the next delegate/middleware in the pipeline.
+            await _next(context);
+        }
     }
 }
