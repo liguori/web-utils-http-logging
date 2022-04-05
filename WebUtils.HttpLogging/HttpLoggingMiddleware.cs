684f261ca52cacf0752f8f2b106d1b5fcd9a8bb5
Gianluigi Liguori <gianluigiliguori@live.it>
2022-04-05 23:59:12 +0200

    First commit

---
+++ WebUtils.HttpLogging/HttpLoggingMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebUtils.HttpLogging
+{
+    internal sealed class HttpLoggingMiddleware
+    {
+    }
+}
---
+++ WebUtils.HttpLogging/HttpLoggingServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace WebUtils.HttpLogging
+{
+    public static class HttpLoggingBuilderExtensions
+    {
+        public static IApplicationBuilder UseWebUtilsHttpLoggingFull(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            app.UseMiddleware<HttpLoggingMiddleware>();
+            return app;
+        }
+    }
+}
